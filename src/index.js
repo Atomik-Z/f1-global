540374e0419068a4cdd91ef118385c619d7116f2
Zakaria Hezouani <atomikdeuxz@gmail.com>
2025-09-17 21:11:59 +0200

    Déploiement - tentative de redirection sur la page home

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { Navigate } from 'react-router-dom'
 import Home from './pages/Home/'
 import Classements from './pages/Classements/Classements'
 import Calendrier from './pages/Calendrier/Calendrier'
